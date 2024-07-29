706a848c799eb2de2f73bc6227fea7896900daca
mostafa2345 <74478034+mostafa2345@users.noreply.github.com>
2024-07-29 14:39:41 +0300

    adding files

---
+++ Data/FirstOneContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FirstOne.Models;
+
+namespace FirstOne.Data
+{
+    public class FirstOneContext : DbContext
+    {
+        public FirstOneContext (DbContextOptions<FirstOneContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstOne.Models.Movie> Movie { get; set; } = default!;
+    }
+}
